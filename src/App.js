967e1ec5c7a644aac5fffe709c36c5e43980d6a5
Arya <ajaarya@apac.corpdir.net>
2022-09-03 22:49:10 +0530

    practise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
-import { About } from "./components/About";
+// import { About } from "./components/About";
+
 import { Navbar } from "./components/Navbar";
 import { OrderSummary } from "./components/OrderSummary";
 import { NoMatch } from "./components/NoMatch";
@@ -12,13 +14,22 @@ import { Users } from "./components/Users";
 import { UserDetails } from "./components/UserDetails";
 import { Admin } from "./components/Admin";
 
+const LazyAbout = React.lazy(() => import("./components/About"));
+
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
+        <Route
+          path="about"
+          element={
+            <React.Suspense fallback="Loading...">
+              <LazyAbout />
+            </React.Suspense>
+          }
+        ></Route>
         <Route path="order-summary" element={<OrderSummary />}></Route>
         <Route path="products" element={<Products />}>
           <Route index element={<FeaturedProducts />}></Route>
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,8 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useSearchParams } from "react-router-dom";
 
 export const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get("filter") === "active";
   return (
     <div>
       <nav>
@@ -9,6 +11,17 @@ export const Users = () => {
         <Link to="3">User 3</Link>
       </nav>
       <Outlet />
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>
+          Active Users
+        </button>
+        <button onClick={() => setSearchParams({})}>Reset Filters</button>
+      </div>
+      {showActiveUsers ? (
+        <h2>Showing active users</h2>
+      ) : (
+        <h2>Showing all users</h2>
+      )}
     </div>
   );
 };
